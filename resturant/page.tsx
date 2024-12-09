b2e931fcff2d4014c12b0be4bd47aee519571c61
Zahid Ullah <vikorzahid@gmail.com>
2024-12-10 00:23:12 +0500

    first commit

--- resturant/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Resturant(){
-    return(
-        
-<div className="font-[sans-serif] py-4 mx-auto lg:max-w-6xl max-w-lg md:max-w-full">
-      <h2 className="text-4xl font-extrabold text-gray-800 mb-12">Foodtuck</h2>
-</div>
-    );
-}
-     
